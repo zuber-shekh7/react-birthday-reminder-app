2e4374bfb3f530ac5857e42598da8bc8dae49d89
Zuber Shekh <zuber.shekh7@gmail.com>
2021-06-30 16:38:28 +0530

    fix: main container height

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,18 @@ import About from "../pages/About";
 import Home from "../pages/Home";
 import Navigation from "./Navigation";
 import Footer from "./Footer";
+import Birthdays from "../pages/Birthdays";
 
 const App = () => {
   return (
     <div>
       <Router>
         <Navigation />
-        <main className="min-vh-100">
+        <main className="mt-4 min-vh-100">
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/about" exact component={About} />
+            <Route path="/birthdays" exact component={Birthdays} />
           </Switch>
         </main>
         <Footer />
