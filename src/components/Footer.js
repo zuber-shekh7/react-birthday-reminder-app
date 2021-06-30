66dd34f2958e0ee160a57f2e85a9558b70c27d9b
Zuber Shekh <zuber.shekh7@gmail.com>
2021-06-30 16:36:22 +0530

    feat: add footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { Container, Col, Row } from "react-bootstrap";
 
 const Footer = () => {
   return (
-    <footer className="bg-dark text-light">
+    <footer className="mt-4 bg-dark text-light">
       <Container>
         <Row>
           <Col className="mx-auto py-4" md={8}>
