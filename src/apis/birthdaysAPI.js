40a18652db85b6bf29cf5c8a20eef4a8f755c9ab
Zuber Shekh <zuber.shekh7@gmail.com>
2021-07-05 21:59:13 +0530

    feat: add create birthday functionality

--- src/apis/birthdaysAPI.js
+++ src/apis/birthdaysAPI.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const birthdaysAPI = axios.create({
   baseURL: "http://127.0.0.1:8000",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default birthdaysAPI;
--- src/pages/AddBirthday.js
+++ src/pages/AddBirthday.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import birthdaysAPI from "../apis/birthdaysAPI";
 import FormCard from "../components/FormCard";
 
 const AddBirthday = () => {
+  const [name, setName] = useState("");
+  const [birthdate, setBirthdate] = useState(null);
+  const history = useHistory();
+
+  const onSubmit = async (e) => {
+    try {
+      const response = await birthdaysAPI.post("birthdays/new/", {
+        name: name,
+        birth_date: birthdate,
+      });
+
+      if (response.status === 201) {
+        history.push("/birthdays");
+      }
+    } catch (error) {
+      alert(JSON.stringify(error.response.data));
+    }
+  };
+
   return (
     <Container>
       <Row>
@@ -14,15 +34,25 @@ const AddBirthday = () => {
           <FormCard title="Add Birthday">
             <Form.Group>
               <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter name" />
+              <Form.Control
+                onChange={(name) => setName(name.target.value)}
+                type="text"
+                placeholder="Enter name"
+              />
               <Form.Text>Enter name of the person</Form.Text>
             </Form.Group>
             <Form.Group>
               <Form.Label>Birth Date</Form.Label>
-              <Form.Control type="date" placeholder="Enter name" />
+              <Form.Control
+                onChange={(date) => setBirthdate(date.target.value)}
+                type="date"
+                placeholder="Enter name"
+              />
               <Form.Text>Select birth date of person</Form.Text>
             </Form.Group>
-            <Button block>Create</Button>
+            <Button onClick={onSubmit} block>
+              Create
+            </Button>
           </FormCard>
         </Col>
       </Row>
