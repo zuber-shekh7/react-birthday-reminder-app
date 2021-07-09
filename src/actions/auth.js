907a25b6eada315db0bad0d832cf809b6007aced
Zuber Shekh <zuber.shekh7@gmail.com>
2021-07-09 15:46:06 +0530

    feat: add signup page

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,28 +1,42 @@
-import { USER_SIGNIN, IS_AUTHENTICATED } from "../types/auth";
-import  authAPI from "../apis/authAPI";
-import  FormData from "form-data";
-
+import { USER_SIGNIN, USER_SIGNIUP, IS_AUTHENTICATED } from "../types/auth";
+import authAPI from "../apis/authAPI";
+import FormData from "form-data";
 
 export const userSignIn = (email, password) => {
-    return async (dispatch) => {
-        const data = new FormData();
-        data.append("email", email)
-        data.append("password", password)
-        try {
-            const response = await authAPI.post('token/',data);
-            localStorage.setItem('accessToken', response.data.access)
-            localStorage.setItem('refreshToken', response.data.refresh)
-            dispatch({type: USER_SIGNIN, payload:response.data})
-        } catch (error) {
-            console.log(error.response.data)
-        }
+  return async (dispatch) => {
+    const data = new FormData();
+    data.append("email", email);
+    data.append("password", password);
+    try {
+      const response = await authAPI.post("accounts/signin/", data);
+      localStorage.setItem("accessToken", response.data.access);
+      localStorage.setItem("refreshToken", response.data.refresh);
+      dispatch({ type: USER_SIGNIN, payload: response.data });
+    } catch (error) {
+      console.log(error.response.data);
     }
-}
+  };
+};
+
+export const userSignUp = (email, password, password2) => {
+  return async (dispatch) => {
+    const data = new FormData();
+    data.append("email", email);
+    data.append("password", password);
+    data.append("password2", password2);
+    try {
+      const response = await authAPI.post("accounts/signup/", data);
+
+      dispatch({ type: USER_SIGNIUP, payload: response.data });
+    } catch (error) {}
+  };
+};
 
 export const isAuthenticated = () => {
-    const token = localStorage.getItem('accessToken')
-    return {
-        type:IS_AUTHENTICATED,
-        payload: {isAuthenticated: token?true:false}
-    }
-}
+  const token = localStorage.getItem("accessToken");
+
+  return {
+    type: IS_AUTHENTICATED,
+    payload: { isAuthenticated: token ? true : false },
+  };
+};
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,38 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { userSignUp } from "../actions/auth";
 import FormCard from "../components/FormCard";
 
-const Signup = () => {
+const Signup = ({ signup }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      return;
+    }
+    signup(email, password, password2);
+  };
+
   return (
     <Container>
       <Row>
         <Col className="mx-auto" md={6}>
-          <FormCard title="Sign up">
-            <Form.Group>
-              <Form.Label>Email Address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text>We won't share your email address</Form.Text>
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Email Password</Form.Label>
-              <Form.Control type="password" placeholder="Enter password" />
-              <Form.Text>We store your password in encrypt form</Form.Text>
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Confirm Password</Form.Label>
-              <Form.Control type="password" placeholder="Re-enter password" />
-              <Form.Text>Enter same password as below</Form.Text>
-            </Form.Group>
-            <Button block>Sign up</Button>
-            <small className="mt-4 d-block text-center">
-              Already have an account? <Link to="/signin">Sign in</Link>
-            </small>
-          </FormCard>
+          <Form onSubmit={handleSubmit}>
+            <FormCard title="Sign up">
+              <Form.Group>
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control
+                  required
+                  onChange={(email) => setEmail(email.target.value)}
+                  type="email"
+                  placeholder="Enter email"
+                />
+                <Form.Text>We won't share your email address</Form.Text>
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Email Password</Form.Label>
+                <Form.Control
+                  required
+                  onChange={(password) => setPassword(password.target.value)}
+                  type="password"
+                  placeholder="Enter password"
+                />
+                <Form.Text>We store your password in encrypt form</Form.Text>
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control
+                  required
+                  onChange={(password2) => setPassword2(password2.target.value)}
+                  type="password"
+                  placeholder="Re-enter password"
+                />
+                <Form.Text>Enter same password as below</Form.Text>
+              </Form.Group>
+              <Button type="submit" block>
+                Sign up
+              </Button>
+              <small className="mt-4 d-block text-center">
+                Already have an account? <Link to="/signin">Sign in</Link>
+              </small>
+            </FormCard>
+          </Form>
         </Col>
       </Row>
     </Container>
   );
 };
 
-export default Signup;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signup: (email, password, password2) =>
+      dispatch(userSignUp(email, password, password2)),
+  };
+};
+export default connect(null, mapDispatchToProps)(Signup);
